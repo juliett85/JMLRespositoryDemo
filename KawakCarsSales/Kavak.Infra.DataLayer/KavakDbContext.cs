e6360e93e5f7b3701524bb2bb95d99e29c387692
Juliett <juliettmartinezlopez@gmail.com>
2021-02-17 18:40:19 -0500

    Edit KavakDbContext

--- KawakCarsSales/Kavak.Infra.DataLayer/KavakDbContext.cs
+++ KawakCarsSales/Kavak.Infra.DataLayer/KavakDbContext.cs
@@ -7,20 +7,6 @@ using System.Text;
 
 namespace Kavak.Infra.DataLayer
 {
-
-    //public class ReservationDbContext : DbContext
-    //{
-       // public ReservationDbContext() { }
-
-       // public ReservationDbContext(DbContextOptions options) : base(options)
-        //{ }
-
-        //public DbSet<Reserve> Reserves { get; set; }
-       // public DbSet<Customer> Customers { get; set; }
-       //public DbSet<CustomerType> CustomerTypes { get; set; }
-
-    //}
-
     public class KavakDbContext : DbContext
     {
         public KavakDbContext() { }
